aa84269834a6791742d1a2debf0e26b095efb05d
Anirban Chatterjee <anirban.chatterjee942@gmail.com>
2019-05-11 12:40:40 +0530

    Code Update

--- LearningGitHub/LearningGitHub/SignUpVC.swift
+++ LearningGitHub/LearningGitHub/SignUpVC.swift
@@ -15,7 +15,8 @@ class SignUpVC: UIViewController {
 
         // Do any additional setup after loading the view.
         print("Signup")
-        print("Signup2")    
+        print("Signup2")
+        print ("SignUp3")
         
     }
     
